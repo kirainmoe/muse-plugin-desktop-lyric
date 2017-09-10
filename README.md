a0b8eaac8485b35ec4a99663651d5b493ef8df1b
kirainmoe <i@kirainmoe.com>
2017-09-10 09:06:15 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # muse-plugin-desktop-lyric
-Adding desktop lyric for your MUSE player, yet an example for building plugins of MUSE.
+
+Adding desktop lyric for your MUSE player, yet an example of building plugins for MUSE.
+
+# Usage
+
+Just import ```muse-plugin-desktop-lyric.js``` and ```muse-plugin-desktop-lyric.css``` to your page.
+
+```html
+<head>
+  <link rel="stylesheet" type="text/css" href="muse-plgin-desktop-lyric.css">
+</head>
+<!-- ...... -->
+<script type="text/javascript" src="muse-player.js"></script>
+<script type="text/javascript" src="muse-plugin-desktop-lyric.js"></script>
+```
+
+If you are going to build a plugin for MUSE, you can refer to this plugin.
---
+++ muse-plugin-desktop-lyric.js
@@ -0,0 +1,75 @@
+/**
+ * MUSE Player plugin: desktop-lyric
+ * Show lyric on fixed position while page is being scrolled.
+ * This is also an official example of developing MUSE plugins.
+ *
+ * @package muse-plygin-desktop-lyric
+ * @author kirainmoe / MoeFront Studio
+ * @version 0.1
+ * @links https://github.com/kirainmoe/muse-plugin-desktop-lyric
+ */
+(function() {
+
+  function MusePluginDesktopLyric() {}
+
+  // disable desktop lyric
+  MusePluginDesktopLyric.prototype.closeDesktopLyric = function() {
+    if (confirm('Disable desktop lyric?')) {
+      document.body.removeChild(this.lyricContainer);
+      window.MUSE.destroyMiddleware('onLyricUpdate', this.onUpdate);
+    }
+  };
+
+  // change position of container
+  MusePluginDesktopLyric.prototype.onMove = function(e) {
+    var width = desktopLyric.lyricContainer.offsetWidth / 2,
+        height = desktopLyric.lyricContainer.offsetHeight / 2;
+    var newPosX = e.clientX - width,
+        newPosY = e.clientY - height;
+    desktopLyric.lyricContainer.style.top = newPosY + 'px';
+    desktopLyric.lyricContainer.style.left = newPosX + 'px';
+  };
+  MusePluginDesktopLyric.prototype.destroyMove = function(e) {
+    desktopLyric.lyricContainer.removeEventListener('mousemove', desktopLyric.onMove);
+    desktopLyric.lyricContainer.removeEventListener('mouseup', desktopLyric.destroyMove);
+  };
+
+  // render desktop lyric to the page
+  MusePluginDesktopLyric.prototype.render = function() {
+    var lyricContainer = document.createElement('div');
+    lyricContainer.setAttribute('class', 'muse-plugin-desktop-lyric__container');
+
+    lyricContainer.addEventListener('contextmenu', this.closeDesktopLyric.bind(this));
+    lyricContainer.addEventListener('mousedown', function(e) {
+      this.lyricContainer.addEventListener('mousemove', this.onMove);
+      this.lyricContainer.addEventListener('mouseup', this.destroyMove);
+    }.bind(this));
+
+    this.lyricContainer = lyricContainer;
+    document.body.appendChild(this.lyricContainer);
+  };
+
+  // handle lyric update event
+  MusePluginDesktopLyric.prototype.onUpdate = function(instance, props) {
+    if (desktopLyric.lyricContainer) {
+      if (desktopLyric.lyricContainer.offsetHeight == 0) {
+        desktopLyric.lyricContainer.style.display = 'block';
+      }
+      if (props.text == '')
+        return;
+      desktopLyric.lyricContainer.innerHTML = props.text;
+    }
+  };
+
+  var desktopLyric = new MusePluginDesktopLyric();
+
+  if (window.MUSE) {
+    MUSE.registerMiddleware('onLyricUpdate', desktopLyric.onUpdate);
+    desktopLyric.render();
+  } else {
+    console.error(
+      'muse-plugin-desktop-lyric should be loaded after MUSE Player Object.'
+    );
+  }
+
+})();
